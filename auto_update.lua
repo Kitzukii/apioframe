6475fc15b2c0a4236811c7483d9a71054ac1e248
Ktzukii <dievissie@gmail.com>
2025-09-09 17:20:23 +0200

    fix

--- auto_update.lua
+++ auto_update.lua
@@ -1,2 +1,3 @@
-local f = "Kitzukii/apioframe/refs/heads/main/client.lua"
-shell.run("wget run https://raw.githubusercontent.com/"..f)
+shell.run("rm ./apioframe/*")
+shell.run("wget https://raw.githubusercontent.com/Kitzukii/apioframe/refs/heads/main/client.lua /apioframe/client.lua")
+shell.run("apioframe/client.lua")
