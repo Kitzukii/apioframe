e7c5e77c8e370d4692ffe7687f03406454120a76
Ktzukii <dievissie@gmail.com>
2025-09-09 17:21:22 +0200

    fgfdsg

--- auto_update.lua
+++ auto_update.lua
@@ -1,3 +1,3 @@
 shell.run("rm ./apioframe/*")
-shell.run("wget https://raw.githubusercontent.com/Kitzukii/apioframe/refs/heads/main/client.lua /apioframe/client.lua")
+shell.run("wget https://raw.githubusercontent.com/Kitzukii/apioframe/refs/heads/main/client.lua ./apioframe/client.lua")
 shell.run("apioframe/client.lua")
